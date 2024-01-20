21b01408d2e837bd28dcff4f7d5f62ad18cdc868
Carson <hello@carson.kiwi>
2024-01-19 22:31:08 -0800

    Update

---
+++ _next/static/TSDsavGeWXJcKMiQT_sJN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/213-0d652d01b2189e60.js","static/chunks/pages/index-4ae73df91655c66d.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
