517d20c96231ac632b637f92b880b952541c8c6e
Carson <hello@carson.kiwi>
2024-02-23 15:05:37 -0500

    Deploy commit

---
+++ _next/static/bMnzS5Cg1nwKSJjwaS0Df/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/213-38dddb4ea109b2fe.js","static/chunks/pages/index-09e371404852b2e6.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
